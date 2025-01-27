d9a8df468f5b7c295bba9405dc5d2e209e290ee3
XuhuaHuang <xuhua.huang.io@gmail.com>
2025-01-26 21:36:25 -0500

    Create params.py

---
+++ src/params.py
@@ -0,0 +1,21 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Contains the hyper parameter required for the project.
+"""
+
+# Camera image hyper parameters
+INIT_EXPOSURE_TIME: int = 12490
+INIT_FRAME_RATE: int = 1
+INIT_GAIN: int = 10
+
+MAX_WIDTH: int = 4196
+MAX_HEIGHT: int = 2128
+
+# Visualization window hyper parameters
+INIT_WIDTH: int = 1080
+INIT_HEIGHT: int = 720
+
+GRABBING_TIMEOUT_MS: int = 5000
