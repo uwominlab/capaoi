ba52ea359e36cd7475e4013f7b36e816b6972d38
XuhuaHuang <xuhua.huang.io@gmail.com>
2025-01-26 23:01:00 -0500

    Update visualize.py

--- utils/visualize.py
+++ utils/visualize.py
@@ -5,24 +5,31 @@ Helper function to show a given image.
 
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def show_img(img_name: str, img: cv2.typing.MatLike) -> None:
+def show_img(img_name: str, img: np.ndarray) -> None:
     """
     Displays an image using matplotlib.
+
+    Args:
         img_name (str): The title of the image to be displayed.
-        img (cv2.typing.MatLike): The image to be displayed. It can be a color image (3 channels) or a grayscale image (1 channel).
+        img (np.ndarray): The image to be displayed. 
+                          Can be a color image (3 channels) or a grayscale image (1 channel).
+
     Returns:
         None
-    Args:
-        img_name (str): _description_
-        img (cv2.typing.MatLike): _description_
     """
-    if len(img.shape) == 3:
-        img = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2RGB)
-        plt.imshow(X=img)
-    else:
-        plt.imshow(img, 'gray')
+    if not isinstance(img, np.ndarray):
+        raise TypeError("The 'img' parameter must be a NumPy ndarray.")
+
+    # Handle color image conversion
+    if len(img.shape) == 3:  # Color image
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        plt.imshow(img)
+    else:  # Grayscale image
+        plt.imshow(img, cmap='gray')
+
     plt.axis('off')
-    plt.title(label=img_name)
+    plt.title(img_name)
     plt.show()
