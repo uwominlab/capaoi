1a4f871519abd1c64dd2e257a553b99a3522d745
LiuRui <776180627@qq.com>
2025-01-30 11:23:41 -0500

    update the

--- src/defects.py
+++ src/defects.py
@@ -12,6 +12,40 @@ import cv2
 MIN_BINARY_THRESH: int = 6
 
 
+def detect_local_defects(capsule_raw, capsule_opened):
+    # (原图掩膜操作>>均值滤波>>滤波图像原图进行差分>>二值化>>查找轮廓(根据轮廓长度进行筛选)
+    draw_image = capsule_raw.copy()
+    capsule_masked = cv2.bitwise_and(draw_image, draw_image, mask=capsule_opened)  # 对原始图像进行掩码运算
+    # 截取胶囊中部视角
+    window = [int(0.35 * capsule_masked.shape[1]), int(0.65 * capsule_masked.shape[1])]
+    capsule_masked = capsule_masked[:, window[0]: window[1], :]
+
+    img1 = capsule_masked
+    img2 = capsule_masked
+    # 中值滤波
+    img1 = cv2.medianBlur(img1, 15)  # 均值滤波
+    diff = cv2.absdiff(img1, img2)  # 图像差分
+
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)  # 二值化
+    minThres = 6
+    _, thres = cv2.threshold(gray, minThres, 255, cv2.THRESH_BINARY)
+    # 提取轮廓
+    contours, hierarchy = cv2.findContours(thres, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    # 缺陷轮廓排序，最大缺陷
+    is_defect = False
+    for i in range(0, len(contours)):
+        length = cv2.arcLength(contours[i], True)
+        # info = info + ">>>>局部缺陷长度: {}".format(length)
+        # 通过轮廓长度筛选
+        if 100 < length:
+            is_defect = True
+            cv2.drawContours(img2, contours[i], -1, (0, 0, 255), 2)
+            # show_img("local_defects", img2)
+
+    return is_defect
+
+
 def detect_capsule_defects(
     capsule_images_raw: list[cv2.typing.MatLike],
     capsule_masks: list[cv2.typing.MatLike],
@@ -19,8 +53,8 @@ def detect_capsule_defects(
     capsule_sizes: list[tuple[int, int]],
     capsule_areas: list[float],
     capsule_similarities: list[float],
-    normal_length_range: tuple[int, int] = (380, 400),
-    normal_area_range: tuple[int, int] = (47000, 50000)
+    normal_length_range: tuple[int, int] = (310, 320),
+    normal_area_range: tuple[int, int] = (30500, 32000)
 ) -> list[tuple[int, int]]:
     """
     Detect defects in capsules based on multiple criteria.
@@ -44,47 +78,37 @@ def detect_capsule_defects(
 
         is_abnormal: bool = False
 
-        # Check if the capsule is too short or too long
+        # 1 >> Check if the capsule is too short or too long
         if not normal_length_range[0] <= length <= normal_length_range[1]:
             abnormal_capsule_centers.append(center)
             is_abnormal = True
             continue
 
-        # Check if the capsule has the proper area
+        # 2 >> Check if the capsule has the proper area
         if not (normal_area_range[0] <= area <= normal_area_range[1]) and not is_abnormal:
             abnormal_capsule_centers.append(center)
             is_abnormal = True
             continue
 
-        # Check for contour similarity
+        # 3 >> Check for contour similarity
         if similarity > 0.1 and not is_abnormal:  # Higher similarity indicates more deviation
             abnormal_capsule_centers.append(center)
             is_abnormal = True
             continue
 
-        # Defect detection
-        masked_image = cv2.bitwise_and(raw_image, raw_image, mask=mask)
-        # Focus on the central region of the capsule
-        width_range = int(
-            0.35 * masked_image.shape[1]), int(0.65 * masked_image.shape[1])
-        central_region = masked_image[:, width_range[0]:width_range[1], :]
+        # 4 >> Defect detection
+        capsule_opened = capsule_set_opened[ii]
+        is_defect = detect_local_defects(capsule_raw, capsule_opened)
+        if is_defect:
+            is_abnormal = 1
 
-        # Perform median filtering and difference computation
-        filtered = cv2.medianBlur(central_region, 15)
-        difference = cv2.absdiff(filtered, central_region)
+        # 打印状态信息 info
+        print(info)
+        # cv2.putText(draw_image, 'Result flag: '+ str(result_flag), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1)
+        # show_img("{}/{} capsules".format(ii, len(capsule_set_raw)), draw_image)
 
-        # Convert to grayscale and threshold
-        gray_diff = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
+        if is_abnormal == 1:
+            centers_abnormal_capsule.append(capsule_centers[ii])
 
-        _, binary_diff = cv2.threshold(gray_diff, MIN_BINARY_THRESH, 255, cv2.THRESH_BINARY)
-        # binary_diff = cv2.adaptiveThreshold(
-        #     gray_diff, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-
-        # Extract and filter contours
-        contours, _ = cv2.findContours(
-            binary_diff, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        for contour in contours:
-            if cv2.arcLength(contour, True) > 50:  # Threshold contour length
-                cv2.drawContours(central_region, [contour], -1, (0, 0, 255), 2)
 
         return abnormal_capsule_centers
