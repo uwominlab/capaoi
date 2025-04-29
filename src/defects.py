f7c673c13c293fb9d72d829b57ab337e3cda6f55
XuhuaHuang <xuhua.huang.io@gmail.com>
2025-04-29 07:54:34 -0400

    Refactor defect detection logic

--- src/defects.py
+++ src/defects.py
@@ -6,98 +6,84 @@
 Module containing functions to detect defects in the given capsule in an image.
 """
 
-import cv2
+import logging
+# pylint: disable=no-name-in-module
+from cv2 import absdiff, arcLength, bitwise_and, cvtColor, findContours, medianBlur, threshold
+from cv2 import COLOR_BGR2GRAY, CHAIN_APPROX_NONE, RETR_EXTERNAL, THRESH_BINARY
+from cv2.typing import MatLike
+
+from PyQt6.QtCore import QSettings
 
 
 MIN_BINARY_THRESH: int = 6
+MAX_LENGTH: int = 0
+
+settings: QSettings = QSettings("MinLab", "CapAOI")
+DEFECTS_DETECTION_DEBUG: bool = settings.value(
+    "defect/debug", type=bool, defaultValue=False)
+logging.basicConfig(
+    level=logging.DEBUG if DEFECTS_DETECTION_DEBUG else logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 
-def detect_defects(raw_image: cv2.typing.MatLike, mask: cv2.typing.MatLike) -> bool:
+def detect_defects(
+    raw_image: MatLike, mask: MatLike, local_defect_length: int
+) -> bool:
     """
     Detect defects in the given capsule image based on the mask.
 
     Args:
-        raw_image (cv2.typing.MatLike): Original capsule image.
-        mask (cv2.typing.MatLike): Binary mask for the capsule.
+        raw_image (MatLike): Original capsule image.
+        mask (MatLike): Binary mask for the capsule.
+        local_defect_length (int): Length threshold for detecting local defects.
 
     Returns:
-        bool: True if a defect is detected, False otherwise.
+        tuple[bool, float]: True if a defect is detected, False otherwise.
+        A float representing the maximum length of the detected defect.
     """
-    masked_image = cv2.bitwise_and(raw_image, raw_image, mask=mask)
+    masked_image = bitwise_and(raw_image, raw_image, mask=mask)
     # Focus on the central region of the capsule
-    width_range = int(
-        0.35 * masked_image.shape[1]), int(0.65 * masked_image.shape[1])
+    width_range: tuple[int, int] = (
+        int(0.40 * masked_image.shape[1]), int(0.60 * masked_image.shape[1]))
     central_region = masked_image[:, width_range[0]:width_range[1], :]
 
     # Perform median filtering and difference computation
-    filtered = cv2.medianBlur(central_region, 15)
-    difference = cv2.absdiff(filtered, central_region)
+    filtered = medianBlur(central_region, 15)
+    difference = absdiff(filtered, central_region)
 
     # Convert to grayscale and threshold
-    gray_diff = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
+    gray_diff = cvtColor(difference, COLOR_BGR2GRAY)
 
-    _, binary_diff = cv2.threshold(
-        gray_diff, MIN_BINARY_THRESH, 255, cv2.THRESH_BINARY)
+    _, binary_diff = threshold(
+        gray_diff, MIN_BINARY_THRESH, 255, THRESH_BINARY)
 
     # Extract and filter contours
-    contours, _ = cv2.findContours(
-        binary_diff, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, _ = findContours(binary_diff, RETR_EXTERNAL, CHAIN_APPROX_NONE)
 
-    is_defect = False
+    is_defect: bool = False
     for contour in contours:
-        if cv2.arcLength(contour, True) > 100:  # Threshold contour length
+        length: float = arcLength(contour, closed=True)
+        if local_defect_length < length:
             is_defect = True
-            cv2.drawContours(central_region, [contour], -1, (0, 0, 255), 2)
 
     return is_defect
 
 
-def detect_local_defects(capsule_raw, capsule_opened):
-    # (原图掩膜操作>>均值滤波>>滤波图像原图进行差分>>二值化>>查找轮廓(根据轮廓长度进行筛选)
-    draw_image = capsule_raw.copy()
-    capsule_masked = cv2.bitwise_and(
-        draw_image, draw_image, mask=capsule_opened)  # 对原始图像进行掩码运算
-    # 截取胶囊中部视角
-    window = [int(0.35 * capsule_masked.shape[1]),
-              int(0.65 * capsule_masked.shape[1])]
-    capsule_masked = capsule_masked[:, window[0]: window[1], :]
-
-    img1 = capsule_masked
-    img2 = capsule_masked
-    # 中值滤波
-    img1 = cv2.medianBlur(img1, 15)  # 均值滤波
-    diff = cv2.absdiff(img1, img2)  # 图像差分
-
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)  # 二值化
-    minThres = 6
-    _, thres = cv2.threshold(gray, minThres, 255, cv2.THRESH_BINARY)
-    # 提取轮廓
-    contours, hierarchy = cv2.findContours(
-        thres, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    # 缺陷轮廓排序，最大缺陷
-    is_defect = False
-    for i in range(0, len(contours)):
-        length = cv2.arcLength(contours[i], True)
-        # info = info + ">>>>局部缺陷长度: {}".format(length)
-        # 通过轮廓长度筛选
-        if 100 < length:
-            is_defect = True
-            cv2.drawContours(img2, contours[i], -1, (0, 0, 255), 2)
-            # show_img("local_defects", img2)
-
-    return is_defect
-
-
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-positional-arguments
+# pylint: disable=too-many-locals
 def detect_capsule_defects(
-    capsule_images_raw: list[cv2.typing.MatLike],
-    capsule_masks: list[cv2.typing.MatLike],
+    capsule_images_raw: list[MatLike],
+    capsule_masks: list[MatLike],
     capsule_centers: list[tuple[int, int]],
     capsule_sizes: list[tuple[int, int]],
     capsule_areas: list[float],
-    capsule_similarities: list[float],
+    capsule_similarities: list[list[float]],
     normal_length_range: tuple[int, int] = (310, 330),
-    normal_area_range: tuple[int, int] = (30500, 34000)
+    normal_area_range: tuple[int, int] = (30500, 35000),
+    similarity_threshold: float = 0.05,
+    local_defect_length: int = 75
 ) -> list[tuple[int, int]]:
     """
     Detect defects in capsules based on multiple criteria.
@@ -110,33 +96,75 @@ def detect_capsule_defects(
     :param capsule_similarities: List of contour similarity scores for each capsule.
     :param normal_length_range: Tuple indicating the normal range of capsule lengths.
     :param normal_area_range: Tuple indicating the normal range of capsule areas.
+    :param similarity_threshold: Threshold for contour similarity.
+    :param local_defect_length: Length threshold for detecting local defects.
     :return: List of centers of capsules flagged as abnormal.
     """
+    # list of abnormal capsule centers, each indicated in the form of a point (x, y)
     abnormal_capsule_centers: list[tuple[int, int]] = []
 
-    for (raw_image, mask, center, size, area, similarity) in zip(
-            capsule_images_raw, capsule_masks, capsule_centers, capsule_sizes, capsule_areas, capsule_similarities):
+    for index, (raw_image, mask, center, size, area, similarities) in enumerate(
+        zip(
+            capsule_images_raw, capsule_masks, capsule_centers,
+            capsule_sizes, capsule_areas, capsule_similarities
+        )
+    ):
 
-        length, _ = size
+        length, width = size
+
+        # Step 0 >> Check if the capsule is already marked as abnormal
+        if center in abnormal_capsule_centers:
+            continue
 
         # Step 1 >> Check if the capsule is too short or too long
-        if not normal_length_range[0] <= length <= normal_length_range[1]:
+        if not normal_length_range[0] <= length <= normal_length_range[1] and \
+                center not in abnormal_capsule_centers:
             abnormal_capsule_centers.append(center)
-            continue
+            if not DEFECTS_DETECTION_DEBUG:
+                continue
 
         # Step 2 >> Check if the capsule has the proper area
-        if not (normal_area_range[0] <= area <= normal_area_range[1]):
+        if not normal_area_range[0] <= area <= normal_area_range[1] and \
+                center not in abnormal_capsule_centers:
             abnormal_capsule_centers.append(center)
-            continue
+            if not DEFECTS_DETECTION_DEBUG:
+                continue
 
         # Step 3 >> Check for contour similarity
-        if similarity > 0.04:  # Higher similarity indicates more deviation
+        # Higher similarity indicates more deviation from the expected shape
+        similarity_overall, similarity_head, similarity_tail = \
+            similarities[0], similarities[1], similarities[2]
+
+        is_abnormal_similarity: bool =\
+            similarity_threshold <= similarity_overall \
+            or 1.5 < similarity_head or 1.5 < similarity_tail
+        if is_abnormal_similarity and center not in abnormal_capsule_centers:
             abnormal_capsule_centers.append(center)
-            continue
+            if not DEFECTS_DETECTION_DEBUG:
+                continue
 
         # Step 4 >> Defect detection
-        is_defect = detect_defects(raw_image, mask)
-        if is_defect:
+        is_defect: bool = detect_defects(
+            raw_image, mask, local_defect_length)
+        if is_defect and center not in abnormal_capsule_centers:
             abnormal_capsule_centers.append(center)
+            if not DEFECTS_DETECTION_DEBUG:
+                continue
 
-        return abnormal_capsule_centers
+        if DEFECTS_DETECTION_DEBUG:
+            info: str = (
+                f"{index + 1} of {len(capsule_images_raw)} capsules:\n"
+                f"Length: {length:.2f} (Normal Range: {normal_length_range}), Width: {width:.2f}\n"
+                f"Length Normal: {normal_length_range[0] <= length <= normal_length_range[1]}\n"
+                f"Area: {area:.2f} (Normal Range: {normal_area_range})\n"
+                f"Area Normal: {normal_area_range[0] <= area <= normal_area_range[1]}\n"
+                # pylint: disable=line-too-long
+                f"Contour Similarity: {similarity_overall:.4f}, {similarity_head:.4f}, {similarity_tail:.4f} (Lower is better)\n"
+                # pylint: disable=line-too-long
+                f"Similarities: {similarity_threshold <= similarity_overall}, {1.5 < similarity_head}, {1.5 < similarity_tail}\n"
+                f"Local Defect Length: {local_defect_length})\n"
+                f"{'>>> Defect Detected <<<\n' if is_defect else '\n'}"
+            )
+            logging.debug(info)
+
+    return abnormal_capsule_centers
