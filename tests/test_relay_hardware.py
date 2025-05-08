61c9b3062b490b8a5fbc08b0456db14d4a155402
XuhuaHuang <xuhua.huang.io@gmail.com>
2025-05-07 20:43:07 -0400

    Run test relay hardware script in factory

--- tests/test_relay_hardware.py
+++ tests/test_relay_hardware.py
@@ -13,9 +13,17 @@ import usb.core
 import usb.util
 from usb.core import Device
 
+# Add parent directory to sys.path if not already present
+# This is necessary to import the src.params module correctly
+import sys
+import os
+if os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) not in sys.path:
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.params import (
     RELAY_1, RELAY_2, RELAY_3, RELAY_4,
-    RELAY_OFF, RELAY_ON, VENDOR_ID, PRODUCT_ID)
+    RELAY_OFF, RELAY_ON, VENDOR_ID, PRODUCT_ID
+)
 
 # Logging configuration block
 from src.relay_controller import RELAY_DEBUG
@@ -36,42 +44,51 @@ class TestRelayHardware(unittest.TestCase):
         """
         Turn on each relay.
         """
-        dev: Device = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
+        dev: Device = usb.core.find(
+            idVendor=VENDOR_ID, idProduct=PRODUCT_ID)  # type: ignore
         if dev is None:
             raise ValueError('Device not found')
 
         logging.debug("Device found: %s", dev)
 
-        dev.ctrl_transfer(bmRequestType=0x21, bRequest=9, wValue=0x200,
-                          wIndex=0, data_or_wLength=[RELAY_ON, RELAY_1, 1])
+        dev.ctrl_transfer(
+            bmRequestType=0x21, bRequest=9, wValue=0x200,
+            wIndex=0, data_or_wLength=[RELAY_ON, RELAY_1, 1])
         logging.debug("Relay turned ON")
         time.sleep(3)
-        dev.ctrl_transfer(bmRequestType=0x21, bRequest=9, wValue=0x200,
-                          wIndex=0, data_or_wLength=[RELAY_OFF, RELAY_1, 1])
+        dev.ctrl_transfer(
+            bmRequestType=0x21, bRequest=9, wValue=0x200,
+            wIndex=0, data_or_wLength=[RELAY_OFF, RELAY_1, 1])
         logging.debug("Relay turned OFF")
 
-        dev.ctrl_transfer(bmRequestType=0x21, bRequest=9, wValue=0x200,
-                          wIndex=0, data_or_wLength=[RELAY_ON, RELAY_2, 1])
+        dev.ctrl_transfer(
+            bmRequestType=0x21, bRequest=9, wValue=0x200,
+            wIndex=0, data_or_wLength=[RELAY_ON, RELAY_2, 1])
         logging.debug("Relay turned ON")
         time.sleep(3)
-        dev.ctrl_transfer(bmRequestType=0x21, bRequest=9, wValue=0x200,
-                          wIndex=0, data_or_wLength=[RELAY_OFF, RELAY_2, 1])
+        dev.ctrl_transfer(
+            bmRequestType=0x21, bRequest=9, wValue=0x200,
+            wIndex=0, data_or_wLength=[RELAY_OFF, RELAY_2, 1])
         logging.debug("Relay turned OFF")
 
-        dev.ctrl_transfer(bmRequestType=0x21, bRequest=9, wValue=0x200,
-                          wIndex=0, data_or_wLength=[RELAY_ON, RELAY_3, 1])
+        dev.ctrl_transfer(
+            bmRequestType=0x21, bRequest=9, wValue=0x200,
+            wIndex=0, data_or_wLength=[RELAY_ON, RELAY_3, 1])
         logging.debug("Relay turned ON")
         time.sleep(3)
-        dev.ctrl_transfer(bmRequestType=0x21, bRequest=9, wValue=0x200,
-                          wIndex=0, data_or_wLength=[RELAY_OFF, RELAY_3, 1])
+        dev.ctrl_transfer(
+            bmRequestType=0x21, bRequest=9, wValue=0x200,
+            wIndex=0, data_or_wLength=[RELAY_OFF, RELAY_3, 1])
         logging.debug("Relay turned OFF")
 
-        dev.ctrl_transfer(bmRequestType=0x21, bRequest=9, wValue=0x200,
-                          wIndex=0, data_or_wLength=[RELAY_ON, RELAY_4, 1])
+        dev.ctrl_transfer(
+            bmRequestType=0x21, bRequest=9, wValue=0x200,
+            wIndex=0, data_or_wLength=[RELAY_ON, RELAY_4, 1])
         logging.debug("Relay turned ON")
         time.sleep(3)
-        dev.ctrl_transfer(bmRequestType=0x21, bRequest=9, wValue=0x200,
-                          wIndex=0, data_or_wLength=[RELAY_OFF, RELAY_4, 1])
+        dev.ctrl_transfer(
+            bmRequestType=0x21, bRequest=9, wValue=0x200,
+            wIndex=0, data_or_wLength=[RELAY_OFF, RELAY_4, 1])
         logging.debug("Relay turned OFF")
 
         usb.util.release_interface(dev, 0)
